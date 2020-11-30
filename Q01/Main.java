c194aa8abb78a1344330dbcc7711007f2aef6391
hirose <hirononari@gmail.com>
2020-11-30 09:14:41 +0900

    課題

--- Q01/Main.java
+++ Q01/Main.java
@@ -4,6 +4,8 @@ public class Main{
 		//要素数5のint配列作成(配列変数名はarr)
 		//index2に10を代入
 		//出力
+		int[] arr=new int[5];
+		arr[3]=10;
 		System.out.println(Arrays.toString(arr));
 	}
 }
--- Q06/Main.java
+++ Q06/Main.java
@@ -4,8 +4,11 @@ public class Main{
 		System.out.print("単語を入れてね>");
 		String word=new Scanner(System.in).next();
 		//文字数をもとにchar配列を作成(配列変数名chars)
+		char[] chars=new char[word.length()];
 		for(int i=0;i<chars.length;i++){
 			//配列にお尻の文字から詰めていく処理
+			char[] c=word.toCharArray();
+			chars[i]=c[chars.length-i];
 		}
 		System.out.println(Arrays.toString(chars));
 	}
